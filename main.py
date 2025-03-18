7ca8b0ef2f7f5081168b9f0f980463921098f2f4
heela03 <165393513+heela03@users.noreply.github.com>
2025-03-18 14:39:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,16 @@ st.set_page_config(
     page_icon="👋"
 )
 
-st.write("# Welcome to Our Page! 👋")
+st.write("# Hey There")
 
 st.write("""
-    This application allows you to predict the sales of items in retail outlets based on various input features.
+    
     
     **Main Features:**
     1. **Sales Prediction**: Predict sales based on item details such as weight, fat content, visibility, and price.
     2. **Data Visualizations**: Explore insightful visualizations that highlight sales performance by outlet type, location, size, and more.
     
-    Use the radio buttons above to navigate through the different sections. Start predicting or explore the data through the visualizations!
     
-    Enjoy using the app, and feel free to experiment with the custom visualization feature to create your own analysis.
     """)
 
 tab1,tab2 = st.tabs(['Predict','Data Visualization'])
